634648f2401f2529a37e5bc50fe66ab1ed50b799
Annette Spyker <aspyker@gmail.com>
2020-09-25 21:07:08 -0500

    Challenge 7 complete - sliding rate limiter

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,13 @@
 package com.redislabs.university.RU102J.dao;
 
+import javafx.beans.binding.ObjectBinding;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+
+import java.util.List;
+import java.util.UUID;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -18,7 +25,22 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     // Challenge #7
     @Override
     public void hit(String name) throws RateLimitExceededException {
-        // START CHALLENGE #7
-        // END CHALLENGE #7
+
+        try(Jedis jedis = jedisPool.getResource()) {
+
+            String key = RedisSchema.getSlidingRateLimiterKey(name, windowSizeMS, maxHits);
+
+            Transaction t = jedis.multi();
+
+            t.zadd(key, System.currentTimeMillis(), UUID.randomUUID().toString());
+            t.zremrangeByScore(key,0, System.currentTimeMillis() - windowSizeMS);
+            Response<Long> count = t.zcard(key);
+
+            t.exec();
+
+            if(count.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
     }
 }
